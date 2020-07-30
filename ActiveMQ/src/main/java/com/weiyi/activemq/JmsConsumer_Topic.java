1a66fe0e674208a61d2a8bb7e8a083ea6e99bf7d
wangwy <weiyiwang404@163.com>
2020-07-30 22:35:04 +0800

    topic持久化。

--- ActiveMQ/src/main/java/com/weiyi/activemq/JmsConsumer_Topic.java
+++ ActiveMQ/src/main/java/com/weiyi/activemq/JmsConsumer_Topic.java
@@ -4,10 +4,11 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.Connection;
 import javax.jms.JMSException;
-import javax.jms.MessageConsumer;
+import javax.jms.Message;
 import javax.jms.Session;
 import javax.jms.TextMessage;
 import javax.jms.Topic;
+import javax.jms.TopicSubscriber;
 
 /**
  * description:
@@ -21,27 +22,24 @@ public class JmsConsumer_Topic {
     public static final String TOPIC_NAME = "topic_one";
 
     public static void main(String[] args) throws JMSException, Exception {
-        System.out.println("*****消费者2*****");
+        System.out.println("*****weiyi*****");
         ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(ACTIVEMQ_URL);
         Connection connection = factory.createConnection();
-        connection.start();
+        // connection.start();
+        // 设置客户端ID。向MQ服务器注册自己的名称
+        connection.setClientID("weiyi");
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
         Topic topic = session.createTopic(TOPIC_NAME);
-        // 5 创建消息的消费者
-        MessageConsumer messageConsumer = session.createConsumer(topic);
-
-        messageConsumer.setMessageListener((message) -> {
-            if (null != message && message instanceof TextMessage) {
-                TextMessage textMessage = (TextMessage) message;
-                try {
-                    System.out.println("listener消费者的消息：" + textMessage.getText());
-                } catch (JMSException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        System.in.read();
-        messageConsumer.close();
+        // 创建一个topic订阅者对象。一参是topic，二参是订阅者名称
+        TopicSubscriber topicSubscriber = session.createDurableSubscriber(topic, "remark...");
+        //  启动连接
+        connection.start();
+        Message message = topicSubscriber.receive();
+        while (null != message) {
+            TextMessage textMessage = (TextMessage) message;
+            System.out.println("收到持久化topic消息：" + textMessage.getText());
+            message = topicSubscriber.receive();
+        }
         session.close();
         connection.close();
     }
